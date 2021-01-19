92bf6593b712535573cf06d3efd49a1731a0d08b
Gabriel De Jesus <gdejes@protonmail.ch>
2021-01-19 14:43:23 -0500

    starting to refactor, more sophisticated and optimized methods, reduce io

--- caesar/caesar.py
+++ caesar/caesar.py
@@ -1,60 +1,76 @@
-# Implementation of traditional 3 shift Caesar cipher
+# Caesar Cipher
 # Author: Gabriel De Jesus
+# Usage:
+#   -s <#> to specify shift amount
+#   -p <fileName> to specify plaintext file to encrypt
+#   -c <fileName> to specify ciphertext to decrypt
+#
+#   example: `python caesar.py -s 3 -p samplefile.txt`
 
 import sys, getopt
 
 def main(argv):
     inputfile = ''
+    shift = 3
     try:
-        opts, args = getopt.getopt(argv, "p:c:", ["ifile="])
+        opts, args = getopt.getopt(argv, "s:p:c:")
     except getopt.GetoptError:
         print ('caesar.py -p <plaintextfile> or caesar.py -c <ciphertextfile>')
         sys.exit(2)
     for opt, arg in opts:
-        high_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        low_alphabet = high_alphabet.lower()
-        shift = 3
-        inputfile = arg
-        # encrypt plaintext with caesar
-        if opt == '-p':
-            f = open(inputfile, 'r')
-            plaintext = f.read()
-            f.close()
-            crypt = []
+        crypt = []
+        # if user specifies shift amount, we'll use it, else we default to 3 character shift
+        if opt == '-s':
+            shift = int(arg)
+        elif opt == '-p':
+            inputfile = arg
             with open(inputfile, 'r') as f:
+                orig = f.read()
+                f.seek(0)
                 for c in f.read():
-                    if c.strip() and c in low_alphabet:
-                        crypt.append(low_alphabet[(low_alphabet.index(c) + shift) % 26])
-                    elif c.strip() and c in high_alphabet:
-                        crypt.append(high_alphabet[(high_alphabet.index(c) + shift) % 26])
-                    else:
-                        crypt.append(c)
+                    cryptor(c,'encrypt',crypt,shift)
                 f.close()
             message = ''.join(crypt)
-            print()
-            print('Original Text: ' + plaintext)
-            print('Encrypted: ' + message)
-            sys.exit()
+            output(orig,message,'enc')
         # decrypt ciphertext
-        if opt == '-c':
-            f = open(inputfile, 'r')
-            ciphertext = f.read()
-            f.close()
-            crypt = []
+        elif opt == '-c':
+            inputfile = arg
             with open(inputfile, 'r') as f:
+                orig = f.read()
+                f.seek(0)
                 for c in f.read():
-                    if c.strip() and c in low_alphabet:
-                        crypt.append(low_alphabet[(low_alphabet.index(c) - shift) % 26])
-                    elif c.strip() and c in high_alphabet:
-                        crypt.append(high_alphabet[(high_alphabet.index(c) - shift) % 26])
-                    else:
-                        crypt.append(c)
+                    cryptor(c,'decrypt',crypt,shift)
                 f.close()
             message = ''.join(crypt)
-            print()
-            print('Original Text: ' + ciphertext)
-            print('Decrypted: ' + message)
-            sys.exit()
+            output(orig,message,'dec')
+
+def cryptor(ch,func,msg,shamt):
+    high_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    low_alphabet = high_alphabet.lower()
+    if func == 'encrypt':
+        if ch.strip() and ch in low_alphabet:
+            msg.append(low_alphabet[(low_alphabet.index(ch) + shamt) % 26])
+        elif ch.strip() and ch in high_alphabet:
+            msg.append(high_alphabet[(high_alphabet.index(ch) + shamt) % 26])
+        else:
+            msg.append(ch)
+    elif func == 'decrypt':
+        if ch.strip() and ch in low_alphabet:
+            msg.append(low_alphabet[(low_alphabet.index(ch) - shamt) % 26])
+        elif ch.strip() and ch in high_alphabet:
+            msg.append(high_alphabet[(high_alphabet.index(ch) - shamt) % 26])
+        else:
+            msg.append(ch)
+    return
+
+def output(orig,out,type):
+    if type == 'enc':
+        tx_t = 'En'
+    elif type == 'dec':
+        tx_t = 'De'
+    print(f"{'Original Text:':<20}{orig:<40}")
+    print(f"{'{}crypted Text:'.format(tx_t):<20}{out:<40}")
+    pass
 
 if __name__ == "__main__":
     main(sys.argv[1:])
