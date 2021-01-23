cb9f925cecf25d663b1fedd38fa52ef614121130
Gabriel De Jesus <gdejes@protonmail.ch>
2021-01-23 14:56:22 -0500

    Fully Functioning Vigenere implementation, todo make functions, clean code, add argument parsing

--- vigenere/vigenere.py
+++ vigenere/vigenere.py
@@ -2,34 +2,36 @@
 
 def main():
     # generate base matrix for vigenere cipher
-    BASE_ALPH = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    print('This is the Base Vigenere Cipher Alphabet Matrix:')
+    BASE_ALPH = r'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    vigenere = []
+    for r in range(0, len(BASE_ALPH)):
+        vigenere.append([0 for c in range(0, len(BASE_ALPH))])
+
     for i in range(0,len(BASE_ALPH)):
         for j in range(0,len(BASE_ALPH)):
             # each iteration, we need to shift the base alphabet by 1
             # we can conveniently repurpose code I wrote for the caesar cipher
-            print(BASE_ALPH[(BASE_ALPH.index(BASE_ALPH[j]) + i) % 26], end="")
+            vigenere[i][j] = BASE_ALPH[(BASE_ALPH.index(BASE_ALPH[j]) + i) % 26]
         print()
 
     # Define a Key and the Message Plaintext (for testing)
     # Eventually, take Key in as script arg, and message parsed from text file.
-    BASE_KEY = 'TAMPA'
-    PLAINTEXT = 'This message is encrypted using the Vigenere Cipher.'
+    BASE_KEY = r'TAMPA'
+    PLAINTEXT = r'THIS MESSAGE IS ENCRYPTED USING THE VIGENERE CIPHER.'
+    # Strip any punctuation or whitespace from the plaintext.
+    PLAINTEXT_STRIP = ''.join(c for c in PLAINTEXT if c.isalnum())
 
     # We need to Repeat the Key to match the length of the plaintext
     #   e.g. TAMPATAMPATA ... until the key reaches "KEY_LEN" in size.
     BASE_KEY_LEN = len(BASE_KEY)
-    KEY_LEN = len(PLAINTEXT)
-    # expandedKey = ''
-    key_index = 0
-    print('\nThis is our expanded cipher key:')
+    KEY_LEN = len(PLAINTEXT_STRIP)
+    keyIndex = 0
+    expandedKey = ""
     for i in range(0,KEY_LEN):
         for j in range(0,BASE_KEY_LEN):
-            if key_index < KEY_LEN:
-                print(BASE_KEY[j], end="")
-                key_index += 1
-            else:
-                return
+            if keyIndex < KEY_LEN:
+                expandedKey += str(BASE_KEY[j])
+                keyIndex += 1
     # In order to encrypt the plaintext, we need to first get the Row index based on the current position within the key
     # then we need to get the column index of the plaintext letter.
     #
@@ -39,5 +41,10 @@ def main():
     #       Third letter of Key is 'M', Third Letter of Plaintext is 'I', so Arr[M][I] would be the Encrypted Char, I -> U
     #   And So on.
     # Eventually, our encrypted message will be: Mhuh mxsepgx ie tnvrketxd ghigg fwe Oistnxrq Riihqg.
+    encoded = ''
+    for i in range(0,KEY_LEN):
+        encoded += vigenere[BASE_ALPH.index(expandedKey[i])][BASE_ALPH.index(PLAINTEXT_STRIP[i])]
+    print("Original Message:" + PLAINTEXT)
+    print("Encoded Message: " + encoded)
 if __name__ == "__main__":
     main()
